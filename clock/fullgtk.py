c270ad70182f94176f5e73e3ce05714114450cde
Amey Kshirsagar <amey@amey-dev-PC.(none)>
2014-02-26 14:47:04 +0530

    Adding new files. Individual web apps

---
+++ clock/fullgtk.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import gtk
+import webkit
+def key_press_event(widget, event) :
+    if event.string == "`":
+        gtk.main_quit()
+window = gtk.Window()
+window.fullscreen()
+window.connect("key-press-event", key_press_event)
+view = webkit.WebView()
+view.open('file:///home/jarvis/clock/index.html')
+window.add(view)
+window.show_all()
+gtk.main()
+
